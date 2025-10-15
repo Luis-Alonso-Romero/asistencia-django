51a427f5e172d9be8e9225f4c03e5aed3294b088
Luis Alonso Romero <luis.romero.s@tecsup.edu.pe>
2025-10-15 10:10:49 -0500

    Agrego dominio de Render a ALLOWED_HOSTS

--- Asistencia/settings.py
+++ Asistencia/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-05e^7_qwpi*^)_lf@*!6&k^r1)-mc0&rb%*vjyo&vy@gzl9j)z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['asistencia-django-f0un.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
